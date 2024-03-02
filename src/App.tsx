5c5030c8c2d79e57c04d5eddd205bb123586786c
Kahon <kahon7586@gmail.com>
2024-03-02 14:03:10 +0800

    build: Initialization

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+    return (
+        <h1 className="bg-black text-xl font-bold leading-6 text-red-500 underline">
+            Hello world!
+        </h1>
+    )
+}
+
+export default App
